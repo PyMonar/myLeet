ded2b7b993a8c4789e31caaecfce4d35403a6a2e
PengYi <pengyic@yonyou.com>
2016-08-23 16:31:42 +0800

    index

--- index.js
+++ index.js
@@ -1,126 +1,112 @@
 /**
- * @param {number} n
- * @return {string[][]}
+ * @param {character[][]} board
+ * @return {void} Do not return anything, modify board in-place instead.
  */
-var result;
-var points;
-var stack;
-var solveNQueens = function(n) {
-    if (n === 0) {
-        return [];
+var copy;
+var solveSudoku = function(board) {
+    copy = [];
+    solve(0, 0, board);
+    for (var i = 0; i < board.length; i++) {
+        for (var j = 0; j < board.length; j++) {
+            board[i][j] = copy[i][j];
+        }
+    }
+};
+
+var solve = function (i, j, board) {
+    if (i > 8) {
+        for (i = 0; i < 9; i++) {
+            copy.push(board[i].slice());
+        }
+        return;
     }
 
-    result = [];
-    points = [];
-    stack = [];
-    var matrix = [];
+    if (board[i][j] !== '.') {
+        i = j === 8 ? i + 1 : i;
+        j = j === 8 ? 0 : j + 1;
+        solve(i, j, board);
+        return;
+    }
+    var row, col;
+    for (var k = 1; k <= 9; k++) {
+        if (check(i, j, board, k + '')) {
+            board[i][j] = k + '';
+            row = j === 8 ? i + 1 : i;
+            col = j === 8 ? 0 : j + 1;
+            solve(row, col, board);
+            board[i][j] = '.';
+        }
+    }
+};
+
+var check = function (row, col, board, val) {
     var i, j;
-    for (i = 0; i < n; i++) {
-        matrix.push([]);
-        for (j = 0; j < n; j++) {
-            matrix[i][j] = '0';
+    for (i = 0; i < board.length; i++) {
+        if (i === col) {
+            continue;
+        }
+        if (board[row][i] === val) {
+            return false;
         }
     }
 
-    solution(matrix, 0);
+    for (i = 0; i < board.length; i++) {
+        if (i === row) {
+            continue;
+        }
+        if (board[i][col] === val) {
+            return false;
+        }
+    }
 
-    return result;
+    var item = getUnit(row, col);
+    var s = item[0], e = item[1];
+    for (i = 0; i < 3; i++) {
+        for (j = 0; j < 3; j++) {
+            if (i === row && j === col) {
+                continue;
+            }
+
+            if (board[i + s][j + e] === val) {
+                return false;
+            }
+        }
+    }
+
+    return true;
 };
 
-var solution = function (matrix, row) {
-    if (row === matrix.length) {
-        // get a solution
-        getSolution(matrix.length);
-        return;
+var getUnit = function (row, col) {
+    var s, e;
+    if (row <= 2) {
+        s = 0;
+    } else if (row <= 5) {
+        s = 3;
+    } else {
+        s = 6;
     }
 
-    if (!hasPlace(matrix, row)) {
-        return;
+    if (col <= 2) {
+        e = 0;
+    } else if (col <= 5) {
+        e = 3;
+    } else {
+        e = 6;
     }
 
-    for (var i = 0; i < matrix.length; i++) {
-        if (matrix[row][i] === '0') {
-            // set points
-            points.push([row, i]);
-            // set matrix and stack
-            var point_num = setMatrix(matrix, row, i);
-            solution(matrix, row + 1);
-            
-            // remove points
-            points.pop();
-            // reset matrix and stack 
-            resetMatrix(matrix, point_num);
-        }
-    }
+    return [s, e];
 };
 
-var setMatrix = function (matrix, row, col) {
-    var j, nums = 0;
-    for (j = 0; j < matrix.length; j++) {
-        if (matrix[row][j] === '0') {
-            matrix[row][j] = '1';
-            stack.push([row, j]);
-            nums++;
-        }
-    }
+var board = [
+    ['5', '3', '.', '.', '7', '.', '.', '.', '.'],
+    ['6', '.', '.', '1', '9', '5', '.', '.', '.'],
+    ['.', '9', '8', '.', '.', '.', '.', '6', '.'],
+    ['8', '.', '.', '.', '6', '.', '.', '.', '3'],
+    ['4', '.', '.', '8', '.', '3', '.', '.', '1'],
+    ['7', '.', '.', '.', '2', '.', '.', '.', '6'],
+    ['.', '6', '.', '.', '.', '.', '2', '8', '.'],
+    ['.', '.', '.', '4', '1', '9', '.', '.', '5'],
+    ['.', '.', '.', '.', '8', '.', '.', '7', '9']
+];
 
-    for (j = row + 1; j < matrix.length; j++) {
-        if (matrix[j][col] === '0') {
-            matrix[j][col] = '1';
-            stack.push([j, col]);
-            nums++;
-        }
-    }
-
-    var left = col - 1, right = col + 1;
-    for (j = row + 1; j < matrix.length; j++) {
-        if (left >= 0 && matrix[j][left] === '0') {
-            matrix[j][left] = '1';
-            stack.push([j, left]);
-            nums++;
-        }
-
-        if (right < matrix.length && matrix[j][right] === '0') {
-            matrix[j][right] = '1';
-            stack.push([j, right]);
-            nums++;
-        }
-        left--;
-        right++;
-    }
-
-    return nums;
-};
-
-var resetMatrix = function (matrix, nums) {
-    while (nums > 0) {
-        var point = stack.pop();
-        matrix[point[0]][point[1]] = '0';
-        nums --;
-    }
-};
-
-var getSolution = function (n) {
-    var solve = [], row;
-    for (var i = 0; i < points.length; i++) {
-        row = [];
-        for (var j = 0; j < n; j++) {
-            row.push('.');
-        }
-        row[points[i][1]] = 'Q';
-        solve.push(row.join(''));
-    }
-    result.push(solve);
-};
-
-var hasPlace = function (matrix, row) {
-    for (var i = 0; i < matrix.length; i++) {
-        if (matrix[row][i] === '0') {
-            return true;
-        }
-    }
-    return false;
-};
-
-
-console.log(solveNQueens(5));
+console.log(solveSudoku(board));
